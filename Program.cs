0f7965fe34c16113ee3438fc1d2c8e0cef657b93
NaveenPulamolu <naveenp992@gmail.com>
2016-12-29 23:02:17 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposableDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User obj = new User("Naveen", "Suma");
+            obj.Dispose();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDisposableDemo
+{
+    public class User : IDisposable
+    {
+        public int id { get; protected set; }
+        public string name { get; protected set; }
+        public string pass { get; protected set; }
+
+        public User(int userID)
+        {
+            id = userID;
+        }
+        public User(string Username, string Password)
+        {
+            name = Username;
+            pass = Password;
+        }
+
+        // Other functions go here...
+
+        public void Dispose()
+        {
+            Console.WriteLine("{0}{1}{2}", name, "+", pass);
+            Dispose(true);
+            GC.SuppressFinalize(this);
+            Console.WriteLine("The value after disposed {0}{1}{2}", name, "+", pass);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+                {
+                    id = 0;
+                    name = String.Empty;
+                    pass = String.Empty;
+                }
+                disposing = true;
+        }
+        ~User() // In Runtime this destructor is treated as a finalize method.
+        {
+            Dispose(false);
+        }
+
+        // GC Does not know about unmanaged resources, such as files and streams. If 
+        // we do not clean them explicitly, we will end up with memory leaks and locked resources.
+    }
+    }
